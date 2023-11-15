b0cd72c51a108be46c06fd03868d48bcbc9e3889
Senichkaa <90269692+Senichkaa@users.noreply.github.com>
2023-11-15 19:59:23 +0000

    Deploying to gh-pages from @ Senichkaa/coursework@729b1ee9043a7a7d2474ebdd085f368ac60f06f2 🚀

---
+++ static/js/69.74cdbcc1.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[69],{69:function(e,t,a){a.r(t);a(791);var r=a(184);t.default=function(){return(0,r.jsx)("div",{children:(0,r.jsx)("p",{children:"Hi, its CatalogPage.jsx"})})}}}]);
+//# sourceMappingURL=69.74cdbcc1.chunk.js.map
---
+++ static/js/899.7ea5785f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[899],{899:function(e,t,r){r.r(t);r(791);var n=r(184);t.default=function(){return(0,n.jsx)("div",{children:(0,n.jsx)("p",{children:"Hi, its ConfigurePage.jsx"})})}}}]);
+//# sourceMappingURL=899.7ea5785f.chunk.js.map
